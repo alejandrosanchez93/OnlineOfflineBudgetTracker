c16df23ba5345e1fbf45a037fcbc8d2d897114bd
Alejandro Sanchez <alejandrosanchezwe@outlook.com>
2020-11-18 17:04:53 -0600

    updated server js file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
@@ -28,8 +28,3 @@ app.listen(PORT, () => {
 });
 
 
-// db.newdatabase.insert({
-//   "name2": "this is name2",
-//   "title": ["item 1", "item2", "item3"],
-//   "country": "this is country"
-// })
